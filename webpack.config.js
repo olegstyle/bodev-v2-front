410b792b63f431e4f8b2da531494805c3c8a934e
Oleh Borysenko <oleg.borisenko@morefromit.com>
2018-08-19 00:49:35 +0200

    more info about IDE helper

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 /**
  * It's just a helper for IDE
+ * If it isn't working then
+ * Add full path to this file in Settings -> Languages & Frameworks > Javascript -> Webpack
  */
 
 const path = require('path');
 
 module.exports = {
   resolve: {
-    extensions: ['.js', '.json', '.vue'],
+    extensions: ['.js', '.json', '.vue', '.png', '.jpg', '.svg'],
     alias: {
       '~': path.resolve(__dirname, './'),
     },

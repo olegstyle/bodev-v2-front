965f1bbf731e4465a71ac5ee3936d542988eef93
Oleh Borysenko <oleg.borisenko@morefromit.com>
2018-08-20 00:41:15 +0200

    lines

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="background">
     <div class="background-left-part"></div>
-    <div class="app-container">
-      <nuxt/>
+    <div class="app-container-wrapper">
+      <div class="line line-type-2 line-position-left"></div>
+      <div class="line line-type-2 line-position-left-top"></div>
+      <div class="line line-type-3 line-position-right-top"></div>
+      <div class="line line-type-2 reverted line-position-right"></div>
+      <div class="line line-type-1 line-position-right-bottom"></div>
+      <div class="line line-type-2 reverted line-position-left-bottom"></div>
+
+      <div class="app-container">
+        <nuxt/>
+      </div>
     </div>
   </div>
 </template>
